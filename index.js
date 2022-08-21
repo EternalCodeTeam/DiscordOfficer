d4df79c738fef8149bda0684e71220a8670e573b
Krzysztof Haller <contact@khaller.com>
2022-08-21 22:10:10 +0200

    :sparkles: Add custom slash command handler, and event handler with example command

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Partials } = require('discord.js');
 const { config } = require('dotenv');
-const { Handler } = require('discord-slash-command-handler');
 // Init .env configuration
 config();
 
 const eternalClient = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ]
+    intents: 32767,
+    partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
 });
 
-eternalClient.on('ready', () => {
-   const slashHandler = new Handler(eternalClient, {
-       commandFolder: "/commands",
-       commandType: "file" || "folder",
-       allSlash: true,
-       autoDefer: true,
-       permissionReply: "Nie posiadasz odpowiednich permisji do korzystania z tego polecenia",
-       errorReply: "Wystąpił błąd wewnętrzny, skontaktuj się z autorem oprogramowania",
-       notOwnerReply: "Tylko twórcy tego oprogramowania mają możliwość z korzystania z tego polecenia."
-   })
-});
+eternalClient.slashCommands = new Collection();
 
-eternalClient.login(process.env.ETERNAL_DISCORD_TOKEN).then(r => console.log("[EternalCode] Client logged successfully!"))
+require('./libs/slashCommandHandler.js')(eternalClient, "commands");
+require('./libs/eventsHandler.js')(eternalClient);
+
+eternalClient.login(process.env.ETERNAL_DISCORD_TOKEN)
---
+++ libs/eventsHandler.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const chalk = require('chalk')
+const AsciiTable = require('ascii-table')
+const table = new AsciiTable()
+table.setHeading('EternalCode.pl -> Events', 'Loaded?').setBorder('|', '=', "-", "-")
+
+module.exports = (client) => {
+    fs.readdirSync('./events/').filter((file) => file.endsWith('.js')).forEach((event) => {
+        require(`../events/${event}`)(client);
+        table.addRow(event.split('.js')[0], '✅')
+    })
+    console.log(chalk.blue(table.toString()))
+};
