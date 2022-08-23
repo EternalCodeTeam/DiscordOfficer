341fb3f5cf7db25b533c3d9f7751a55c71ec6e61
Krzysztof Haller <contact@khaller.com>
2022-08-23 16:57:44 +0200

    :sparkles: Add banCommand with modal, refactor eventHandler

---
+++ commands/administration/banCommand.js
@@ -0,0 +1,44 @@
+const { ApplicationCommandType, GuildMember, PermissionsBitField, EmbedBuilder} = require('discord.js');
+const { Modal, TextInputComponent, showModal} = require('discord-modals');
+module.exports = {
+    name: 'ban',
+    description: "Use ban hammer on user",
+    default_member_permissions: "BanMembers",
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'user',
+            description: 'Select the user who got ban',
+            type: 6,
+            required: true,
+        }
+    ],
+    run: async (client, interaction) => {
+        if(interaction.options.get('user').member.permissions.has(PermissionsBitField.Flags.Administrator) || interaction.options.get('user').member.permissions.has(PermissionsBitField.Flags.BanMembers))
+            return interaction.reply({ ephemeral: true, embeds: [
+                    new EmbedBuilder()
+                        .setDescription(`🚫 ${interaction.user}, Nie możesz wyrzucić użytkownika \`${interaction.options.get('user').user.username}\`!`)
+                        .setColor('Red')
+                ]})
+
+        const banModal = new Modal()
+            .setCustomId('ban-modal')
+            .setTitle('Ban member from this server')
+            .addComponents(
+                new TextInputComponent()
+                    .setCustomId('user-banned')
+                    .setStyle('SHORT')
+                    .setLabel("Don't modify this input!")
+                    .setRequired(true)
+                    .setDefaultValue(interaction.options.get('user').user.id),
+                new TextInputComponent()
+                    .setCustomId('ban-reason')
+                    .setLabel('Reason')
+                    .setStyle('LONG')
+                    .setRequired(false)
+                    .setPlaceholder('Write here reason of the ban')
+            )
+
+        await showModal(banModal, {client, interaction});
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Collection, Partials } = require('discord.js');
 const { config } = require('dotenv');
+const discordModals = require('discord-modals');
 // Init .env configuration
 config();
 
@@ -7,7 +8,7 @@ const eternalClient = new Client({
     intents: 32767,
     partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
 });
-
+discordModals(eternalClient);
 eternalClient.slashCommands = new Collection();
 
 require('./libs/slashCommandHandler.js')(eternalClient, "commands");
