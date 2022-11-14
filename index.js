3965d881b5ac3fb334aa8bce8f8373f3764e4035
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2022-11-14 07:36:37 +0100

    Add global logger

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-const { Client, Collection, IntentsBitField } = require("discord.js");
+const { Client, Collection } = require("discord.js");
 const { config } = require("dotenv");
-const discordModals = require("discord-modals");
 config();
 
-const eternalClient = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.DirectMessages,
-        IntentsBitField.Flags.DirectMessageReactions,
-        IntentsBitField.Flags.MessageContent,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessageReactions,
-        IntentsBitField.Flags.GuildBans,
-        IntentsBitField.Flags.GuildVoiceStates
-    ]
-});
+const eternalClient = new Client({ intents: 32767 });
 
-discordModals(eternalClient);
 eternalClient.slashCommands = new Collection();
+
+global.logger = require("./utils/Logger");
 
 require("./handler/slashCommandHandler.js")(eternalClient, "commands");
 require("./handler/eventsHandler.js")(eternalClient);
 require("./events/music/initPlayer.js")(eternalClient);
 
-eternalClient.login(process.env.ETERNAL_DISCORD_TOKEN);
+eternalClient.login(process.env.ETERNAL_DISCORD_TOKEN).then(r => {
+    logger.info("Logged in as " + eternalClient.user.tag);
+});
