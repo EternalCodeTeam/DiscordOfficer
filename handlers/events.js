88c43fe8efc7439655a2e259695a2902aee8e0f3
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2022-11-19 21:14:54 +0100

    More improve handlers

--- handlers/events.js
+++ handlers/events.js
@@ -3,8 +3,11 @@ const fs = require("fs");
 module.exports = (client) => {
     try {
         fs.readdirSync("./events/").filter((file) => file.endsWith(".js")).forEach((file) => {
-            const filePath = `../events/${file}`;
-            const event = require(filePath);
+            const event = require(`../events/${file}`);
+
+            if (!("name" in event && "once" in event && "execute" in event)) {
+                logger.warn(`The command at ${file} is missing a required "data", "once" or "execute" property.`);
+            }
 
             if (event.once) {
                 client.once(event.name, (...args) => event.execute(...args));
@@ -12,7 +15,7 @@ module.exports = (client) => {
                 client.on(event.name, (...args) => event.execute(...args));
             }
 
-            delete require.cache[require.resolve(filePath)];
+            delete require.cache[require.resolve(`../events/${file}`)];
         });
 
         logger.info("All events successfully loaded.");
