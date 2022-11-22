2931ab7f48e75ce102f1bc182d06439c43f28254
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2022-11-22 01:26:01 +0100

    Create say.js

---
+++ slashCommands/info/say.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { PermissionFlagsBits, ChannelType } = require("discord-api-types/v10");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("say")
+        .setDescription("Make the bot say something")
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addStringOption(option => option.setName("message").setDescription("The message").setRequired(true))
+        .addChannelOption(option => option.setName("channel").setDescription("The channel").setRequired(false).addChannelTypes(ChannelType.GuildText)),
+    execute: async (interaction) => {
+        const message = interaction.options.getString("message");
+        const channel = interaction.options.getChannel("channel") || interaction.channel;
+
+
+        interaction.reply({ content: `Message sent in <#${channel.id}>`, ephemeral: true });
+        await channel.send(message);
+    }
+}
