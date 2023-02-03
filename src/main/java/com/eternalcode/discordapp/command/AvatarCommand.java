e05e31c1c5fe6893f9539041cc84b0cb2f188f04
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2023-02-03 22:18:36 +0100

    Remove redundant imports

--- src/main/java/com/eternalcode/discordapp/command/AvatarCommand.java
+++ src/main/java/com/eternalcode/discordapp/command/AvatarCommand.java
@@ -1,12 +1,10 @@
 package com.eternalcode.discordapp.command;
 
-import com.freya02.botcommands.api.annotations.UserPermissions;
 import com.freya02.botcommands.api.application.ApplicationCommand;
 import com.freya02.botcommands.api.application.annotations.AppOption;
 import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
 import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import org.jetbrains.annotations.NotNull;
