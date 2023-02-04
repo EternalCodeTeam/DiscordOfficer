466f1d3c6287abd90ff5679a2e9ea651810b06c8
hyd3r1 <contact@khaller.com>
2023-02-04 16:49:51 +0100

    Update dockerimages and add embed command

--- java17.dockerfile
+++ java17.dockerfile
@@ -1,3 +1,9 @@
+LABEL org.label-schema.name="eternalcode/discordofficer"
+LABEL org.label-schema.description="EternalCode DiscordOfficer official image"
+LABEL org.label-schema.url="http://eternalcode.pl/"
+LABEL org.label-schema.vcs-url="https://github.com/EternalCodeTeam/DiscordOfficer/issues"
+LABEL org.label-schema.docker.cmd="docker run -v $(pwd)/eternalofficer:/home/eternalcode/ -d eternalcode/discordofficer"
+
 FROM gradle:7.6.0-jdk-alpine AS build
 
 WORKDIR /app/
---
+++ src/main/java/com/eternalcode/discordapp/command/EmbedCommand.java
@@ -0,0 +1,51 @@
+package com.eternalcode.discordapp.command;
+
+import com.eternalcode.discordapp.Embeds;
+import com.freya02.botcommands.api.annotations.Optional;
+import com.freya02.botcommands.api.annotations.UserPermissions;
+import com.freya02.botcommands.api.application.ApplicationCommand;
+import com.freya02.botcommands.api.application.annotations.AppOption;
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+@UserPermissions(Permission.MANAGE_CHANNEL)
+public class EmbedCommand extends ApplicationCommand {
+
+    @JDASlashCommand(
+            name = "embed",
+            description = "Send embed message"
+    )
+    public void onSlashCommand(
+            @NotNull GuildSlashEvent event,
+            @NotNull @AppOption(name = "title") String title,
+            @NotNull @AppOption(name = "description") String description,
+            @Optional @AppOption(name = "color") String color,
+            @Optional @AppOption(name = "image-url") String image
+    ) {
+        try {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle(title);
+            embed.setDescription(description);
+            embed.setColor(Color.decode(color));
+            embed.setThumbnail(image);
+
+            MessageEmbed messageEmbed = embed.build();
+
+            event.getChannel().sendMessageEmbeds(messageEmbed);
+        } catch (Exception ignored) {
+            MessageEmbed embed = new Embeds().error
+                    .setDescription("I do not have the permission to ban this user!")
+                    .build();
+
+            event.replyEmbeds(embed)
+                    .setEphemeral(true)
+                    .queue();
+        }
+    }
+}
