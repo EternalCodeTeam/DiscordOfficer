6d4a6b42fcf7ec69d74d772448f0bb4aec6d8e24
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-02-23 17:53:37 +0100

    Add say command

---
+++ src/main/java/com/eternalcode/discordapp/command/SayCommand.java
@@ -0,0 +1,30 @@
+package com.eternalcode.discordapp.command;
+
+import com.jagrosh.jdautilities.command.SlashCommand;
+import com.jagrosh.jdautilities.command.SlashCommandEvent;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.List;
+
+public class SayCommand extends SlashCommand {
+
+    public SayCommand() {
+        this.name = "say";
+        this.help = "Says something";
+        this.userPermissions = new Permission[]{ Permission.MESSAGE_MANAGE };
+        this.options = List.of(
+                new OptionData(OptionType.STRING, "message", "the message to say")
+                        .setRequired(true)
+        );
+    }
+
+    @Override
+    public void execute(SlashCommandEvent event) {
+        String message = event.getOption("message").getAsString();
+
+        event.getChannel().sendMessage(message).queue();
+        event.reply("Sent the message").setEphemeral(true).queue();
+    }
+}
