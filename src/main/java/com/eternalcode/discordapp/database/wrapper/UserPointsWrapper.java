57a2784b899b0fbb5e5a4cd1071e5089c69cb025
hyd3r1 <contact@khaller.com>
2023-03-03 16:03:47 +0100

    Repair formatting in UserPointsWrapper

--- src/main/java/com/eternalcode/discordapp/database/wrapper/UserPointsWrapper.java
+++ src/main/java/com/eternalcode/discordapp/database/wrapper/UserPointsWrapper.java
@@ -7,25 +7,25 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = "edc_user_points")
 public class UserPointsWrapper {
 
-        @DatabaseField(id = true)
-        private Long id;
+    @DatabaseField(id = true)
+    private Long id;
 
-        @DatabaseField(columnName = "points", defaultValue = "0")
-        private int points;
+    @DatabaseField(columnName = "points", defaultValue = "0")
+    private int points;
 
-        public UserPointsWrapper() {
-        }
+    public UserPointsWrapper() {
+    }
 
-        public UserPointsWrapper(Long id, int points) {
-            this.id = id;
-            this.points = points;
-        }
+    public UserPointsWrapper(Long id, int points) {
+        this.id = id;
+        this.points = points;
+    }
 
-        public static UserPointsWrapper from(UserPoints userPoints) {
-            return new UserPointsWrapper(userPoints.getId(), userPoints.getPoints());
-        }
+    public static UserPointsWrapper from(UserPoints userPoints) {
+        return new UserPointsWrapper(userPoints.getId(), userPoints.getPoints());
+    }
 
-        public UserPoints toUserPoints() {
-            return new UserPoints(this.id, this.points);
-        }
+    public UserPoints toUserPoints() {
+        return new UserPoints(this.id, this.points);
+    }
 }
