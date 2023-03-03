067f1a614b2184f4688a09093fe656e291bc18b2
hyd3r1 <contact@khaller.com>
2023-03-03 15:50:54 +0100

    Sort dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,19 +32,15 @@ dependencies {
     // new modern fork of jda-utilities
     implementation("pw.chew:jda-chewtils-command:2.0-SNAPSHOT")
 
-    // ORMLite Core
-    implementation("com.j256.ormlite:ormlite-core:6.1")
 
-    // ORMLite JDBC
+    // ORMLite
+    implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
 
-    // MySQL JDBC Driver
+    // Database drivers
     implementation("mysql:mysql-connector-java:8.0.32")
-    // PostgreSQL JDBC Driver
     implementation("org.postgresql:postgresql:42.5.3")
-    // https://mvnrepository.com/artifact/com.h2database/h2
     implementation("com.h2database:h2:2.1.214")
-    // MariaDB JDBC Driver
     implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
 }
 
