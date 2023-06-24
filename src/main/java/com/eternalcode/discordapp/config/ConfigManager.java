a96524b1825fd17243e54c35eae81cfb2a010f9e
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-06-24 14:52:39 +0200

    Add ConfigManager to git

---
+++ src/main/java/com/eternalcode/discordapp/config/ConfigManager.java
@@ -0,0 +1,38 @@
+package com.eternalcode.discordapp.config;
+
+import com.eternalcode.discordapp.composer.InstantComposer;
+import net.dzikoysk.cdn.Cdn;
+import net.dzikoysk.cdn.CdnFactory;
+import net.dzikoysk.cdn.reflect.Visibility;
+
+import java.io.File;
+import java.time.Instant;
+
+public class ConfigManager {
+
+    private static final Cdn CDN = CdnFactory
+            .createYamlLike()
+            .getSettings()
+            .withComposer(Instant.class, new InstantComposer())
+            .withMemberResolver(Visibility.PRIVATE)
+            .build();
+
+    private final File folder;
+
+    public ConfigManager(String directory) {
+        this.folder = new File(directory);
+    }
+
+    public <T extends CdnConfig> void load(T dataFile) {
+        CDN.load(dataFile.resource(this.folder), dataFile)
+                .orThrow(RuntimeException::new);
+
+        CDN.render(dataFile, dataFile.resource(this.folder))
+                .orThrow(RuntimeException::new);
+    }
+
+    public <T extends CdnConfig> void save(T config) {
+        CDN.render(config, config.resource(this.folder))
+                .orThrow(RuntimeException::new);
+    }
+}
