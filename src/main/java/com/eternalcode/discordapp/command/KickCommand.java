dad03ff60e4b09f6586109280bf8a1674efb8649
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-10-16 20:39:52 +0200

    Revert.

--- src/main/java/com/eternalcode/discordapp/command/KickCommand.java
+++ src/main/java/com/eternalcode/discordapp/command/KickCommand.java
@@ -16,9 +16,6 @@ import java.util.List;
 
 public class KickCommand extends SlashCommand {
 
-    private static final String REQUESTED_BY = "Requested by ";
-    private static final String REASON = "reason";
-
     private final AppConfig appConfig;
 
     public KickCommand(AppConfig appConfig) {
@@ -27,10 +24,10 @@ public class KickCommand extends SlashCommand {
         this.userPermissions = new Permission[] { Permission.KICK_MEMBERS };
 
         this.options = List.of(
-                new OptionData(OptionType.USER, "user", "select the user")
-                        .setRequired(true),
-                new OptionData(OptionType.STRING, REASON, "provide a reason")
-                        .setRequired(false)
+            new OptionData(OptionType.USER, "user", "select the user")
+                .setRequired(true),
+            new OptionData(OptionType.STRING, "reason", "provide a reason")
+                .setRequired(false)
         );
 
         this.appConfig = appConfig;
@@ -40,65 +37,65 @@ public class KickCommand extends SlashCommand {
     public void execute(SlashCommandEvent event) {
         try {
             User user = event.getOption("user").getAsUser();
-            String reason = event.getOption(REASON) != null ? event.getOption(REASON).getAsString() : "No reason provided";
+            String reason = event.getOption("reason") != null ? event.getOption("reason").getAsString() : "No reason provided";
 
             if (user.isBot()) {
                 MessageEmbed embed = new EmbedBuilder()
-                        .setTitle("❌ | An error occurred while kicking the user")
-                        .setColor(Color.decode(this.appConfig.embedSettings.errorEmbed.color))
-                        .setThumbnail(this.appConfig.embedSettings.errorEmbed.thumbnail)
-                        .setDescription("You can't kick a bot")
-                        .setFooter(REQUESTED_BY + event.getUser().getName(), event.getUser().getAvatarUrl())
-                        .setTimestamp(Instant.now())
-                        .build();
+                    .setTitle("❌ | An error occurred while kicking the user")
+                    .setColor(Color.decode(this.appConfig.embedSettings.errorEmbed.color))
+                    .setThumbnail(this.appConfig.embedSettings.errorEmbed.thumbnail)
+                    .setDescription("You can't kick a bot")
+                    .setFooter("Requested by " + event.getUser().getAsTag(), event.getUser().getAvatarUrl())
+                    .setTimestamp(Instant.now())
+                    .build();
 
                 event.replyEmbeds(embed)
-                        .setEphemeral(true)
-                        .queue();
+                    .setEphemeral(true)
+                    .queue();
 
                 return;
             }
 
             user.openPrivateChannel().queue(channel -> {
                 MessageEmbed embed = new EmbedBuilder()
-                        .setTitle("🔨 | You have been kicked from " + event.getGuild().getName())
-                        .setColor(Color.decode(this.appConfig.embedSettings.errorEmbed.color))
-                        .setThumbnail(this.appConfig.embedSettings.errorEmbed.thumbnail)
-                        .setDescription(REASON + reason)
-                        .setTimestamp(Instant.now())
-                        .build();
+                    .setTitle("🔨 | You have been kicked from " + event.getGuild().getName())
+                    .setColor(Color.decode(this.appConfig.embedSettings.errorEmbed.color))
+                    .setThumbnail(this.appConfig.embedSettings.errorEmbed.thumbnail)
+                    .setDescription("Reason: " + reason)
+                    .setTimestamp(Instant.now())
+                    .build();
 
                 channel.sendMessageEmbeds(embed).submit();
             });
 
             MessageEmbed embed = new EmbedBuilder()
-                    .setTitle("✅ | Successfully kicked " + user.getName())
-                    .setColor(Color.decode(this.appConfig.embedSettings.successEmbed.color))
-                    .setThumbnail(this.appConfig.embedSettings.successEmbed.thumbnail)
-                    .setDescription(REASON + reason)
-                    .setFooter(REQUESTED_BY + event.getUser().getName(), event.getUser().getAvatarUrl())
-                    .setTimestamp(Instant.now())
-                    .build();
+                .setTitle("✅ | Successfully kicked " + user.getAsTag())
+                .setColor(Color.decode(this.appConfig.embedSettings.successEmbed.color))
+                .setThumbnail(this.appConfig.embedSettings.successEmbed.thumbnail)
+                .setDescription("Reason: " + reason)
+                .setFooter("Requested by " + event.getUser().getAsTag(), event.getUser().getAvatarUrl())
+                .setTimestamp(Instant.now())
+                .build();
 
             event.replyEmbeds(embed)
-                    .setEphemeral(true)
-                    .queue();
+                .setEphemeral(true)
+                .queue();
 
             event.getGuild().kick(user).reason(reason).queue();
         }
         catch (Exception exception) {
             MessageEmbed embed = new EmbedBuilder()
-                    .setTitle("❌ | An error occurred while kicking the user")
-                    .setColor(Color.decode(this.appConfig.embedSettings.errorEmbed.color))
-                    .setThumbnail(this.appConfig.embedSettings.errorEmbed.thumbnail)
-                    .setDescription("I can't kick this user, he probably has highest role than me!")
-                    .setFooter(REQUESTED_BY + event.getUser().getName(), event.getUser().getAvatarUrl())
-                    .setTimestamp(Instant.now())
-                    .build();
+                .setTitle("❌ | An error occurred while kicking the user")
+                .setColor(Color.decode(this.appConfig.embedSettings.errorEmbed.color))
+                .setThumbnail(this.appConfig.embedSettings.errorEmbed.thumbnail)
+                .setDescription("I can't kick this user, he probably has highest role than me!")
+                .setFooter("Requested by " + event.getUser().getAsTag(), event.getUser().getAvatarUrl())
+                .setTimestamp(Instant.now())
+                .build();
 
             event.replyEmbeds(embed)
-                    .setEphemeral(true)
-                    .queue();
+                .setEphemeral(true)
+                .queue();
         }
     }
 }
