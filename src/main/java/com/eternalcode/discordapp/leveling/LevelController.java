06923867cc1d3341ba137d5a2ff7dce557aeb2de
vluckyyy <vluckyyy.biznes@gmail.com>
2023-10-25 15:21:52 +0200

    Move level-related calculations to LevelUtil.

--- src/main/java/com/eternalcode/discordapp/leveling/LevelController.java
+++ src/main/java/com/eternalcode/discordapp/leveling/LevelController.java
@@ -3,6 +3,7 @@ package com.eternalcode.discordapp.leveling;
 import com.eternalcode.discordapp.leveling.experience.Experience;
 import com.eternalcode.discordapp.leveling.experience.ExperienceChangeEvent;
 import com.eternalcode.discordapp.observer.Observer;
+import com.eternalcode.discordapp.util.LevelUtil;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
@@ -20,24 +21,6 @@ public class LevelController implements Observer<ExperienceChangeEvent> {
         this.jda = jda;
     }
 
-    /**
-     * This method is called when an ExperienceChangeEvent occurs. It updates the user's level based on their experience points
-     * and sends a message to a designated TextChannel when the user levels up.
-     * <p>
-     * It calculates the next level using the formula: N = P * L^2 + P * L + P
-     * where:
-     * <p> - N represents the points needed to reach the next level,
-     * <p> - P is the points needed for one level (provided by LevelConfig),
-     * <p> - L is the current level.
-     * <p>
-     * Example level calculations:
-     * <p> Level 1: N = 100 * 1^2 + 100 * 1 + 100 = 300
-     * <p> Level 2: N = 100 * 2^2 + 100 * 2 + 100 = 700
-     * <p> Level 3: N = 100 * 3^2 + 100 * 3 + 100 = 1200
-     * <p> Level 4: N = 100 * 4^2 + 100 * 4 + 100 = 1700
-     * <p> Level 5: N = 100 * 5^2 + 100 * 5 + 100 = 2300
-     */
-
     @Override
     public void update(ExperienceChangeEvent experienceChangeEvent) {
         Experience experience = experienceChangeEvent.experience();
@@ -50,9 +33,7 @@ public class LevelController implements Observer<ExperienceChangeEvent> {
         this.levelService.find(userId).thenApply(userLevel -> {
             int currentLevel = userLevel.getCurrentLevel();
 
-            int pointsNeededForNextLevel = (int) (pointsNeededForOneLevel * Math.pow(currentLevel + 1, 2)
-                + pointsNeededForOneLevel * (currentLevel + 1)
-                + pointsNeededForOneLevel);
+            int pointsNeededForNextLevel = LevelUtil.calculatePointsForNextLevel(currentLevel, pointsNeededForOneLevel);
 
             if (experiencePoints <= pointsNeededForNextLevel) {
                 return null;
