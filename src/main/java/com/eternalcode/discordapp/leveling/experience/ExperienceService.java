8499bff7a39974750961e2873bdd3420cbc87458
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2024-06-26 21:29:42 +0200

    Close responses. Fix potential leaks.

--- src/main/java/com/eternalcode/discordapp/leveling/experience/ExperienceService.java
+++ src/main/java/com/eternalcode/discordapp/leveling/experience/ExperienceService.java
@@ -3,7 +3,6 @@ package com.eternalcode.discordapp.leveling.experience;
 import com.eternalcode.discordapp.database.DatabaseManager;
 import com.eternalcode.discordapp.observer.ObserverRegistry;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.LongSupplier;
 
@@ -17,24 +16,12 @@ public class ExperienceService {
         this.observerRegistry = observerRegistry;
     }
 
-    public CompletableFuture<Experience> saveExperience(Experience experience, LongSupplier channelId) {
-        return this.experienceRepository.saveExperience(experience).whenComplete((experience1, throwable) -> {
-            if (throwable == null) {
-                this.observerRegistry.publish(new ExperienceChangeEvent(experience1, channelId));
-            }
-        });
-    }
-
     public CompletableFuture<Experience> modifyPoints(long userId, double points, boolean add, LongSupplier channelId) {
         return this.experienceRepository.modifyPoints(userId, points, add).whenComplete((experience, throwable) -> {
             if (throwable == null) {
                 this.observerRegistry.publish(new ExperienceChangeEvent(experience, channelId));
             }
         });
-    }
-
-    public CompletableFuture<List<Experience>> getTop(int limit, long offset) {
-        return this.experienceRepository.getTop(limit, offset);
     }
 
 }
--- src/main/java/com/eternalcode/discordapp/user/UserRepositoryException.java
+++
@@ -1,8 +0,0 @@
-package com.eternalcode.discordapp.user;
-
-public class UserRepositoryException extends RuntimeException {
-
-    public UserRepositoryException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
