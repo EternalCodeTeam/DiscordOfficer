46ba408ed3b1624d8eda4d2a52aa19fb8b153d46
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-02-15 06:30:26 +0100

    build: migrate Java version config to Gradle toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,8 +82,9 @@ tasks.withType<JavaCompile> {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks.shadowJar {
